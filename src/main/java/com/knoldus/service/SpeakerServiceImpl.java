b261ea2316792ab56e50f04cf178bd904c564604
Nitin Mishra <nitin.mishra@knoldus.com>
2021-04-12 10:56:02 +0530

    Conference app with constructor injection

--- src/main/java/com/knoldus/service/SpeakerServiceImpl.java
+++ src/main/java/com/knoldus/service/SpeakerServiceImpl.java
@@ -8,6 +8,13 @@ import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
     private SpeakerRepository repository;
+    //constructor injection - index
+    public SpeakerServiceImpl() {
+
+    }
+    public SpeakerServiceImpl(SpeakerRepository repository) {
+        this.repository = repository;
+    }
 
     public List<Speaker>  findAll() {
         return repository.findAll();
