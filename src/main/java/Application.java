5c4cbc6194ffb2bcc531280c53c9d91c9150dbe1
Nitin Mishra <nitin.mishra@knoldus.com>
2021-04-12 10:48:45 +0530

    Conference app with bean configuration

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,13 @@
 import com.knoldus.service.SpeakerService;
 import com.knoldus.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
     public static void main(String[] args) {
-        //wire up SpeakerService
-        SpeakerService service = new SpeakerServiceImpl();
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //SpeakerService service = new SpeakerServiceImpl();
+        SpeakerService service = applicationContext.getBean("speakerService",SpeakerService.class);
         System.out.println(service.findAll().get(0).getFirstName());
     }
 }
--- src/main/java/com/knoldus/service/SpeakerServiceImpl.java
+++ src/main/java/com/knoldus/service/SpeakerServiceImpl.java
@@ -1,14 +1,17 @@
 package com.knoldus.service;
 
 import com.knoldus.model.Speaker;
-import com.knoldus.repository.HibernateSpeakerRepositoryImpl;
 import com.knoldus.repository.SpeakerRepository;
 
 import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
-    private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+    private SpeakerRepository repository; //pain point -- hard coded -- eliminated in Spring
     public List<Speaker>  findAll() {
         return repository.findAll();
+    }
+
+    public void setSpeakerRepository(SpeakerRepository repository) {
+        this.repository = repository;
     }
 }
